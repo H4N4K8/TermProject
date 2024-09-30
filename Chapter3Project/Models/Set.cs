378392dea897a0c65b3a7fb993e0fe7d56a35b21
H4N4K8 <hannah.francis03@gmail.com>
2024-09-29 23:42:11 -0400

    ch4 updates

---
+++ Chapter3Project/Models/Set.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Chapter3Project.Models
+{
+    public class Set
+    {
+        public string Id { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Please enter set name.")]
+        [Display(Name ="Set Name")]
+        public string SetName { get; set; } = string.Empty;
+        [Display(Name = "Smiski Pose")]
+        public string SmiskiType { get; set; } = string.Empty;
+
+    }
+}
--- Chapter3Project/Program.cs
+++ Chapter3Project/Program.cs
@@ -1,7 +1,13 @@
+using Chapter3Project.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<SetContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("movieContext")));
 
 var app = builder.Build();
 
